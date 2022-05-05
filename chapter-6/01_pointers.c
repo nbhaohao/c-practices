7b73fc828d6f9197d6fba85ce9afe8f24f68e57f
nbhaohao <nbhaohao@163.com>
2022-05-05 14:31:21 +0800

    chapter-6

--- chapter-6/01_pointers.c
+++ chapter-6/01_pointers.c
@@ -4,7 +4,7 @@ int main()
 {
     int i = 72;
     int j = 723;
-    printf("The address of i is %u and %u\n", &i, &j);
-    printf("The value of i and j is %u and %u\n", *(&i), *(&j));
+    printf("The address of i is %p and %p\n", &i, &j);
+    printf("The value of i and j is %d and %d\n", *(&i), *(&j));
     return 0;
 }
